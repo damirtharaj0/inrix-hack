911939ea31fca3a946039f4771f79ebe27428dcd
dannyaam9 <dannyaam9@gmail.com>
2022-11-12 12:01:47 -0800

    loaded google map

---
+++ src/Map.js
@@ -0,0 +1,34 @@
+import { GoogleMap, useLoadScript, } from '@react-google-maps/api'
+import React from 'react'
+
+
+const mapContainerStyle = {
+    width: '100vw',
+    height: '100vh',
+};
+
+const center = {
+    lat: 37.7749,
+    lng : -122.4194,
+};
+
+export default function Map() {
+
+    const {isLoaded, loadError} = useLoadScript({
+        googleMapsApiKey: "AIzaSyAEDd9dKasrXfKiKJT3RrDyzvzADacz1MI",
+    });
+
+    if (loadError) return "Error loading Map"
+    if (!isLoaded) return "Loading Maps"
+
+
+  return (
+    <div>
+        <GoogleMap
+            mapContainerStyle={mapContainerStyle}
+            zoom={12}
+            center={center}>
+        </GoogleMap>
+    </div>
+  )
+}
