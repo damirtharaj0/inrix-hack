baeefe8349d7de7effd16648062944afcdec3ea8
dannyaam9 <dannyaam9@gmail.com>
2022-11-12 16:21:37 -0800

    address form

--- src/App.js
+++ src/App.js
@@ -4,12 +4,105 @@ import getRoute from "./Api";
 
 function App() {
 
+  let token = ''
+
   const start = "37.770581%2C-122.442550"
   const end = "37.765297%2C-122.442527"
+
+  let address1 = ''
+  let zip1 = ''
+  let address2 = ''
+  let zip2 = ''
+
+  function parseAddress() {
+
+    if (!document.getElementById('address1').value) return;
+    if (!document.getElementById('zip1').value) return;
+    if (!document.getElementById('address2').value) return;
+    if (!document.getElementById('zip2').value) return;
+
+    address1 = document.getElementById('address1').value
+    zip1 = document.getElementById('zip1').value
+    address2 = document.getElementById('address2').value
+    zip2 = document.getElementById('zip2').value
+  }
+
+  function getCoordinates() {
+    let address1Arr = address1.split(' ');
+    if (address1Arr.length < 2) return;
+    let url1 = `https://api.geoapify.com/v1/geocode/search?housenum=${address1Arr[0]}&street=${address1Arr.slice(1).join('%20')}&city=San%20Francisco&zip=${zip1}&state=CA&country=United%20States&apiKey=9b0a76ae68984a9da39f81ec480d0a17`;
+    // console.log(url1);
+    fetch(url1).then(res => res.json()).then(data => {
+      let long1 = data['features'][0]['bbox'][0]
+      let lat1 = data['features'][0]['bbox'][1]
+
+    })
+
+
+    let address2Arr = address2.split(' ');
+    if (address2Arr.length < 2) return;
+    let url2 = `https://api.geoapify.com/v1/geocode/search?housenum=${address2Arr[0]}&street=${address2Arr.slice(1).join('%20')}&city=San%20Francisco&zip=${zip2}&state=CA&country=United%20States&apiKey=9b0a76ae68984a9da39f81ec480d0a17`;
+    // console.log(url2);
+    fetch(url2).then(res => res.json()).then(data => {
+      let long2 = data['features'][0]['bbox'][0]
+      let lat2 = data['features'][0]['bbox'][1]
+
+    })
+
+  }
+
+  function getRoute(start, end) {
+    const url = `https://api.iq.inrix.com/findRoute?wp_1=${start}&wp_2=${end}&format=json&accessToken=${token}`;
+  
+    const data = {
+        method : 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    };
+    
+    fetch(url, data).then(res => res.json())
+  }
+
+  function callApi() {
+    let appId = "z4limdy5g3";
+    let hashToken = "ejRsaW1keTVnM3w4bE9sODVPM1FDNk1UdWpaMWlIM2E1bExoR243SmxBTXI3ZXpFbFI3";
+    let url = `https://api.iq.inrix.com/auth/v1/appToken?appId=${appId}&hashToken=${hashToken}`;
+
+    const data = {
+      method : 'GET',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+    };
+    var requestOptions = {
+      method: 'GET'
+    };
+
+    let response = fetch(url, requestOptions).then(res => res.json()).then(data => {
+      token = data.result.token;
+      console.log(token)
+    });
+  }
+
+  callApi()
   getRoute(start, end);
 
   return (
-    <Map />
+    <>
+      Address :<input id='address1'></input>
+      <br/>
+      Zip : <input id='zip1'></input>
+      <br/>
+      Address :<input id='address2'></input>
+      <br/>
+      Zip : <input id='zip2'></input>
+      <button onClick={event => {
+        parseAddress();
+        getCoordinates();
+      }}>Search</button>
+      <Map />
+    </>
   );
 };
 
--- src/Map.js
+++ src/Map.js
@@ -1,20 +1,17 @@
 import { GoogleMap, useLoadScript, } from '@react-google-maps/api'
 import React from 'react'
 
-
-const mapContainerStyle = {
-    width: '100vw',
-    height: '100vh',
-};
-
-const center = {
-    lat: 37.7749,
-    lng : -122.4194,
-};
-
 export default function Map() {
 
-
+    const mapContainerStyle = {
+        width: '100vw',
+        height: '100vh',
+    };
+    
+    const center = {
+        lat: 37.7809841,
+        lng : -122.4132879,
+    };
 
     const {isLoaded, loadError} = useLoadScript({
         googleMapsApiKey: "AIzaSyAEDd9dKasrXfKiKJT3RrDyzvzADacz1MI",
@@ -28,7 +25,7 @@ export default function Map() {
     <div>
         <GoogleMap
             mapContainerStyle={mapContainerStyle}
-            zoom={12}
+            zoom={20}
             center={center}>
         </GoogleMap>
     </div>
